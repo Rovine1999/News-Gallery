261d1bdc890a5b3eba1e1fce6dfd99a1185f0e8b
Rovine1999 <rovinewanjala99@gmail.com>
2020-11-30 18:43:52 +0300

    add modified lines on macros.html,news.html and confi.py

--- app/config.py
+++ app/config.py
@@ -3,6 +3,7 @@ class Config:
     General configuration parent class
     '''
     NEWS_API_BASE_URL ='https://newsapi.org/v2/sources?apiKey=5d2b0a6a455c4d4db1029ce0d678f050'
+    NEWS_API_ARTICLES_URL='https://newsapi.org/v2/everything?q=bitcoin&apiKey=5d2b0a6a455c4d4db1029ce0d678f050'
 
 
 
