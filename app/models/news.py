44fa506d542eb4b256ac66607ee72c7e0d06af45
Rovine1999 <rovinewanjala99@gmail.com>
2020-11-30 22:56:11 +0300

    add card to images

--- app/models/news.py
+++ app/models/news.py
@@ -3,7 +3,7 @@ class News:
     News class to define News Objects
     '''
 
-    def __init__(self,id, name, description, url, category, language, country):
+    def __init__(self,id, name, description, url, category, language, country,image):
         self.id =id
         self.name = name
         self.description = description
@@ -11,3 +11,4 @@ class News:
         self.category = category
         self.language = language
         self.country = country
+        self.image = image
