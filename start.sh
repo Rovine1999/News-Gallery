d5591ddf71fca5659e684017ae66c221a003bbdc
Rovine1999 <rovinewanjala99@gmail.com>
2020-12-01 23:54:24 +0300

    add modified start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 
-export NEWS_API_KEY = '5d2b0a6a455c4d4db1029ce0d678f050'
-export SECRET_KEY ='ROVINE'
+export NEWS_API_KEY='5d2b0a6a455c4d4db1029ce0d678f050'
+export SECRET_KEY='ROVINE'
 
 python3.8 manage.py server
